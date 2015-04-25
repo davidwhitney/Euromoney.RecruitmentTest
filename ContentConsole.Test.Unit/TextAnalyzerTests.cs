98e34672bbcf08673de7cb00cee7d615e44f58b8
Dan Cohen <dan.cohen@mopowered.co.uk>
2015-04-25 11:04:53 +0100

    Added tests - red test on repeat instances of same bad word - add regex

---
+++ ContentConsole.Test.Unit/TextAnalyzerTests.cs
@@ -0,0 +1,48 @@
+﻿namespace ContentConsole.Test.Unit
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class TextAnalyzerTests
+    {
+        [Test]
+        public void GivenEmptyString_Returns0()
+        {
+            var input = string.Empty;
+
+            var result = TextAnalyzer.CountBadWords(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void GivenOneBadWord_Returns1()
+        {
+            const string input = "bad";
+
+            var result = TextAnalyzer.CountBadWords(input);
+
+            Assert.AreEqual(1, result);
+        }
+
+        [Test]
+        public void GivenOneNotBadWord_Returns1()
+        {
+            const string input = "hello";
+
+            var result = TextAnalyzer.CountBadWords(input);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void Given3BadWordsAnd3NotBadWord_Returns3()
+        {
+            const string input = "bad bad bad good good good";
+
+            var result = TextAnalyzer.CountBadWords(input);
+
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ ContentConsole/TextAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿namespace ContentConsole
+{
+    public static class TextAnalyzer
+    {
+        private const string BannedWord1 = "swine";
+        private const string BannedWord2 = "bad";
+        private const string BannedWord3 = "nasty";
+        private const string BannedWord4 = "horrible";
+
+        public static int CountBadWords(string text)
+        {
+
+            int badWords = 0;
+            if (text.Contains(BannedWord1))
+            {
+                badWords = badWords + 1;
+            }
+            if (text.Contains(BannedWord2))
+            {
+                badWords = badWords + 1;
+            }
+            if (text.Contains(BannedWord3))
+            {
+                badWords = badWords + 1;
+            }
+            if (text.Contains(BannedWord4))
+            {
+                badWords = badWords + 1;
+            }
+
+            return badWords;
+        }
+    }
+}
