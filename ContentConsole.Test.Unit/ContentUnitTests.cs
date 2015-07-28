c51f0879b2c2bacdd938c623813a583b170b2cd4
JWebb96 <jlp.webb1@gmail.com>
2015-07-28 11:21:28 +0100

    Commit after test completion
    Added .Core project
    Added unit tests
    Added logic to command line project

---
+++ ContentConsole.Test.Unit/ContentUnitTests.cs
@@ -0,0 +1,385 @@
+﻿namespace ContentConsole.Test.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using ContentConsole.Core;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// A class containing all the
+    /// unit tests to be run.
+    /// </summary>
+    [TestFixture]
+    public class ContentUnitTests
+    {
+        private SharedMethod shared;
+
+        public ContentUnitTests()
+        {
+            shared = new SharedMethod();
+        }
+
+        #region Story 1
+
+        /// <summary>
+        /// Simple test as a user
+        /// using the default sentence.
+        /// </summary>
+        [Test]
+        public void BasicUserTest()
+        {
+            // Arrange            
+            int predictedCount = 2;
+            string sentence = "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+
+            // Act
+            int actualCount = shared.CountBannedWords(sentence);
+
+            // Assert
+            Assert.AreEqual(predictedCount, actualCount);
+        }
+
+        /// <summary>
+        /// Test where all words in
+        /// the sentence are banned.
+        /// </summary>
+        [Test]
+        public void AllBannedWordsUserTest()
+        {
+            // Arrange
+            int predictedCount = 4;
+            string sentence = "Swine, bad, nasty, horrible.";
+
+            // Act
+            int actualCount = shared.CountBannedWords(sentence);
+
+            // Assert
+            Assert.AreEqual(predictedCount, actualCount);
+        }
+
+        /// <summary>
+        /// Test where there are
+        /// no banned words.
+        /// </summary>
+        [Test]
+        public void NoBannedWordsUserTest()
+        {
+            // Arrange
+            int predictedCount = 0;
+            string sentence = "There are no banned words in this sentence.";
+
+            // Act
+            int actualCount = shared.CountBannedWords(sentence);
+
+            // Assert
+            Assert.AreEqual(predictedCount, actualCount);
+        }
+
+        /// <summary>
+        /// Test passing an empty string
+        /// through as a sentence.
+        /// </summary>
+        [Test]
+        public void EmptyStringUserTest()
+        {
+            // Arrange
+            int predictedCount = 0;
+            string sentence = string.Empty;
+
+            // Act
+            int actualCount = shared.CountBannedWords(sentence);
+
+            // Assert
+            Assert.AreEqual(predictedCount, actualCount);
+        }
+
+        #endregion Story 1
+
+        #region Story 2
+
+        /// <summary>
+        /// Simple test as an admin
+        /// where the list of banned
+        /// words will be retrieved.
+        /// </summary>
+        [Test]
+        public void BasicAdminListTest()
+        {
+            // Arrange
+            List<string> predictedOutput = new List<string> { "swine", "bad", "nasty", "horrible" };
+
+            // Act
+            List<string> actualOutput = shared.PrintBannedList();
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+        }
+
+        /// <summary>
+        /// Simple test as an admin
+        /// where the list of banned
+        /// words will be retrieved
+        /// but the predicted output
+        /// will be different.
+        /// </summary>
+        [Test]
+        public void NotEqualAdminListTest()
+        {
+            // Arrange
+            List<string> predictedOutput = new List<string> { "these", "are", "not", "the", "right", "words" };
+
+            // Act
+            List<string> actualOutput = shared.PrintBannedList();
+
+            // Assert
+            Assert.AreNotEqual(predictedOutput, actualOutput);
+        }
+
+        /// <summary>
+        /// Adding one value to the list
+        /// </summary>
+        [Test]
+        public void BasicAdminAddToListTest()
+        {
+            // Arrange
+            List<string> wordToAdd = new List<string> { "negative" };
+            List<string> predictedOutput = new List<string> { "swine", "bad", "nasty", "horrible", "negative" };
+
+            // Act
+            List<string> actualOutput = shared.AddWordsToBannedList(wordToAdd);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Adding multiple values to the list
+        /// </summary>
+        [Test]
+        public void AdminAddMultipleToListTest()
+        {
+            // Arrange
+            List<string> wordsToAdd = new List<string> { "negative", "disgust", "banned" };
+            List<string> predictedOutput = new List<string> { "swine", "bad", "nasty", "horrible", "negative", "disgust", "banned" };
+
+            // Act
+            List<string> actualOutput = shared.AddWordsToBannedList(wordsToAdd);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Adding null value to the list
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AdminAddNullToListTest()
+        {
+            // Arrange
+            List<string> wordToAdd = new List<string> { null };            
+
+            // Act
+            List<string> actualOutput = shared.AddWordsToBannedList(wordToAdd);
+
+            // Assert
+            // Nothing to assert as it should throw an exception
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Removing one value from the list
+        /// </summary>
+        [Test]
+        public void BasicAdminRemoveFromListTest()
+        {
+            // Arrange
+            List<string> wordToRemove = new List<string> { "bad" };
+            List<string> predictedOutput = new List<string> { "swine", "nasty", "horrible" };
+
+            // Act
+            List<string> actualOutput = shared.RemoveWordsFromBannedList(wordToRemove);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Removing multiple values from the list
+        /// </summary>
+        [Test]
+        public void AdminRemoveMultipleFromListTest()
+        {
+            // Arrange
+            List<string> wordsToRemove = new List<string> { "swine", "bad" };
+            List<string> predictedOutput = new List<string> { "nasty", "horrible" };
+
+            // Act
+            List<string> actualOutput = shared.RemoveWordsFromBannedList(wordsToRemove);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Removing all the values from the list
+        /// </summary>
+        [Test]
+        public void AdminRemoveAllFromListTest()
+        {
+            // Arrange
+            List<string> wordsToRemove = new List<string> { "swine", "bad", "nasty", "horrible" };
+            List<string> predictedOutput = new List<string> { "List of banned words is empty." };
+
+            // Act
+            List<string> actualOutput = shared.RemoveWordsFromBannedList(wordsToRemove);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Removing null value from the list
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AdminRemoveNullFromListTest()
+        {
+            // Arrange
+            List<string> wordToRemove = new List<string> { null };
+
+            // Act
+            List<string> actualOutput = shared.RemoveWordsFromBannedList(wordToRemove);
+
+            // Assert
+            // Nothing to assert as it should throw an exception
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Removing value from the list
+        /// that isn't present.
+        /// </summary>
+        [Test]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AdminRemoveWrongValueFromListTest()
+        {
+            // Arrange
+            List<string> wordToRemove = new List<string> { "asdf" };
+
+            // Act
+            List<string> actualOutput = shared.RemoveWordsFromBannedList(wordToRemove);
+
+            // Assert
+            // Nothing to assert as it should throw an exception
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        /// <summary>
+        /// Clearing the list of banned words
+        /// </summary>
+        [Test]
+        public void AdminClearListTest()
+        {
+            // Arrange
+            string predictedOutput = "List of banned words successfully cleared.";
+
+            // Act
+            string actualOutput = shared.ClearBannedList();
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+
+            // Reset list so it doesn't affect the other tests
+            shared.ResetBannedList();
+        }
+
+        #endregion Story 2
+
+        #region Story 3
+
+        /// <summary>
+        /// Simple test as a reader
+        /// using the default sentence.
+        /// </summary>
+        [Test]
+        public void BasicReaderTest()
+        {
+            // Arrange
+            string sentence = "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            string predictedOutput = "the weather in manchester in winter is b#d. it rains all the time - it must be h######e for people visiting.";
+
+            // Act
+            string actualOutput = shared.MaskBannedWords(sentence);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+        }
+
+        /// <summary>
+        /// Simple test as a reader
+        /// using all banned words.
+        /// </summary>
+        [Test]
+        public void AllBannedWordsReaderTest()
+        {
+            // Arrange
+            string sentence = "Swine, bad, nasty, horrible.";
+            string predictedOutput = "s###e, b#d, n###y, h######e.";
+
+            // Act
+            string actualOutput = shared.MaskBannedWords(sentence);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+        }
+
+        /// <summary>
+        /// Simple test as a reader
+        /// using no banned words.
+        /// </summary>
+        [Test]
+        public void NoBannedWordsReaderTest()
+        {
+            // Arrange
+            string sentence = "There are no banned words in this sentence.";
+            string predictedOutput = "there are no banned words in this sentence.";
+
+            // Act
+            string actualOutput = shared.MaskBannedWords(sentence);
+
+            // Assert
+            Assert.AreEqual(predictedOutput, actualOutput);
+        }
+
+        #endregion Story 3
+
+        // Not sure if this was needed
+        // as the requirements for Story
+        // 1 & 4 seem to be identical.
+        // (From my understanding anyways.)
+        #region Story 4
+        #endregion Story 4
+    }
+}
--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -1,44 +1,153 @@
-﻿using System;
-
-namespace ContentConsole
+﻿namespace ContentConsole
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using ContentConsole.Core;
+
     public static class Program
     {
         public static void Main(string[] args)
         {
-            string bannedWord1 = "swine";
-            string bannedWord2 = "bad";
-            string bannedWord3 = "nasty";
-            string bannedWord4 = "horrible";
+            // Should probably be an enum, not sure how to implement yet
+            int userLevel;
+            SharedMethod shared = new SharedMethod();
 
-            string content =
-                "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            Console.WriteLine("Content Editor");
+            Console.WriteLine("\r\n1 - User");
+            Console.WriteLine("2 - Administrator");
+            Console.WriteLine("3 - Reader");
+            Console.WriteLine("4 - Content Curator");
 
-            int badWords = 0;
-            if (content.Contains(bannedWord1))
+            Console.WriteLine("\r\nPlease select the number that corresponds to your access level. ");
+            bool validNumber = int.TryParse(Console.ReadLine(), out userLevel);
+
+            if (validNumber == false || userLevel > 4 || userLevel < 1)
             {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord2))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord3))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord4))
-            {
-                badWords = badWords + 1;
+                Console.WriteLine("You have selected an invalid access level, press ANY key to try again.");
+                Console.ReadLine();
+                Console.Clear();
+                Program.Main(new string[] { });
             }
 
-            Console.WriteLine("Scanned the text:");
-            Console.WriteLine(content);
-            Console.WriteLine("Total Number of negative words: " + badWords);
+            if (userLevel == 2)
+            {
+                AdminTools();
+            }
+            else
+            {
+                List<string> sentences = new List<string> {
+                "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting."};
 
-            Console.WriteLine("Press ANY key to exit.");
+                foreach (string sentence in sentences)
+                {
+                    int badWordCount = shared.CountBannedWords(sentence);
+
+                    Console.WriteLine("\r\nScanned the text:");
+
+                    if (badWordCount == 0)
+                    {
+                        Console.WriteLine(sentence);
+                    }
+                    else
+                    {
+                        if (userLevel != 1 && userLevel != 4)
+                        {
+                            Console.WriteLine(shared.MaskBannedWords(sentence));
+                        }
+                        else
+                        {
+                            Console.WriteLine(sentence);
+                        }
+                    }
+
+                    if (userLevel != 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Total Number of negative words: " + badWordCount);
+                        Console.ResetColor();
+                    }
+                }
+            }
+
+            Console.WriteLine("\r\nPress ANY key to exit.");
             Console.ReadKey();
         }
-    }
 
+        /// <summary>
+        /// The method that is called
+        /// if the user is an admin.
+        /// </summary>
+        private static void AdminTools()
+        {
+            SharedMethod shared = new SharedMethod();
+            List<string> bannedList = shared.PrintBannedList();
+            int selectedOption;
+
+            PrintBannedList(bannedList);
+
+            Console.WriteLine("\r\n1 - Add word(s) to list");
+            Console.WriteLine("2 - Remove word(s) from list");
+            Console.WriteLine("3 - Clear list");
+            Console.WriteLine("Please select an option from the above.");
+            bool validOption = int.TryParse(Console.ReadLine(), out selectedOption);
+
+            if (validOption == false || selectedOption > 3 || selectedOption < 1)
+            {
+                Console.WriteLine("You have selected an invalid option, press ANY key to try again.");
+                Console.ReadLine();
+                Program.AdminTools();
+            }
+
+            if (selectedOption == 1)
+            {
+                Console.WriteLine("Please type the words to add to the list as comma seperated values.");
+
+                List<string> wordsToAdd = new List<string>();
+                wordsToAdd.AddRange(Console.ReadLine().Split(','));
+
+                shared.AddWordsToBannedList(wordsToAdd);
+                PrintBannedList(bannedList);
+            }
+            else if (selectedOption == 2)
+            {
+                Console.WriteLine("Please type the words to remove from the list as comma seperated values.");
+
+                List<string> wordsToRemove = new List<string>();
+                wordsToRemove.AddRange(Console.ReadLine().Split(','));
+
+                shared.RemoveWordsFromBannedList(wordsToRemove);
+                PrintBannedList(bannedList);
+            }
+            else if (selectedOption == 3)
+            {
+                Console.WriteLine(shared.ClearBannedList());
+            }
+        }
+
+        /// <summary>
+        /// Prints the current list of
+        /// banned words to the console.
+        /// </summary>
+        /// <param name="bannedList">The list of banned words.</param>
+        private static void PrintBannedList(List<string> bannedList)
+        {
+            int wordCount = 0;
+            Console.WriteLine("\r\nThe list of banned words are:");
+
+            foreach (string word in bannedList)
+            {
+                wordCount++;
+
+                if (wordCount == bannedList.Count)
+                {
+                    Console.WriteLine(word);
+                }
+                else
+                {
+                    Console.WriteLine(word + ",");
+                }
+            }
+        }
+    }
 }
