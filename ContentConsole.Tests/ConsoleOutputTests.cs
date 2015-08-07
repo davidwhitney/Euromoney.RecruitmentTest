bfc010f8178afdb7c78e7884e003fdd4b5598b9d
ChrisWorledge <chrisw1@gmx.com>
2015-08-07 11:57:49 +0100

    Modified as requested
    Implemented the four scenarios described in the brief using a second
    class with separate methods for each case.

---
+++ ContentConsole.Tests/ConsoleOutputTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentConsole;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace ContentConsole.Tests
+{
+    [TestClass()]
+    public class ConsoleOutputTests
+    {
+        [TestMethod()]
+        public void ConsoleOutputTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void CheckWordsTest()
+        {
+            int expected = 2;
+            string[] badWords = new string[] { "swine", "bad", "nasty", "horrible" };
+            string content =
+               "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            ConsoleOutput twoBad = new ConsoleOutput(badWords, content, false);
+            int actual = twoBad.CheckWords();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void HideBadWordsTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void ShowBadWordsTest()
+        {
+            
+        }
+
+        [TestMethod()]
+        public void SpaceText()
+        {
+            
+        }
+    }
+}
--- ContentConsole/Program.cs
+++ ContentConsole/Program.cs
@@ -6,39 +6,38 @@ namespace ContentConsole
     {
         public static void Main(string[] args)
         {
-            string bannedWord1 = "swine";
-            string bannedWord2 = "bad";
-            string bannedWord3 = "nasty";
-            string bannedWord4 = "horrible";
+            string[] badWords = new string[] { "swine", "bad", "nasty", "horrible" };//can be obtained from datastore getBadWords() would call a stored procedure to return the latest set of bad words
+            /*Along these line sorry this one happens to be MySql, but they are pretty similar
+             public DataTable getQuestions(int categoryID)
+        {
+            DataTable dt = new DataTable();
+            string query = "sp_getQuestions";
 
+            using (MySqlConnection con = new MySqlConnection(cGlobal.CatsConnectionString))
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("category", categoryID);
+                cmd.ExecuteNonQuery();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                da.Fill(dt);
+                con.Close();
+            }
+            return dt;
+        }*/
             string content =
-                "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
-
-            int badWords = 0;
-            if (content.Contains(bannedWord1))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord2))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord3))
-            {
-                badWords = badWords + 1;
-            }
-            if (content.Contains(bannedWord4))
-            {
-                badWords = badWords + 1;
-            }
-
-            Console.WriteLine("Scanned the text:");
-            Console.WriteLine(content);
-            Console.WriteLine("Total Number of negative words: " + badWords);
-
+               "The weather in Manchester in winter is bad. It rains all the time - it must be horrible for people visiting.";
+            ConsoleOutput noBad = new ConsoleOutput(badWords, content, false);//The trueShow boolean could be provided from a datastore
+            noBad.SpaceText();
+            ConsoleOutput withBad = new ConsoleOutput(badWords, content, true);//You wouldn't normally call them both!
+            Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("Press ANY key to exit.");
             Console.ReadKey();
         }
+
     }
 
 }
