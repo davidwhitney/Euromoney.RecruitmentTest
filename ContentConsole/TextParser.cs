b5452b8ff2faadeddbe27c51bb8736d8279574e9
Gabriel Nepomuceno <gnepomuceno@veinteractive.com>
2015-09-07 21:53:45 +0100

    Minimum refactor to enable tests

---
+++ ContentConsole/TextParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentConsole
+{
+    public class TextParser
+    {
+        public int CountBadWords(string content)
+        {
+            string bannedWord1 = "swine";
+            string bannedWord2 = "bad";
+            string bannedWord3 = "nasty";
+            string bannedWord4 = "horrible";
+
+            int badWords = 0;
+            if (content.Contains(bannedWord1))
+            {
+                badWords = badWords + 1;
+            }
+            if (content.Contains(bannedWord2))
+            {
+                badWords = badWords + 1;
+            }
+            if (content.Contains(bannedWord3))
+            {
+                badWords = badWords + 1;
+            }
+            if (content.Contains(bannedWord4))
+            {
+                badWords = badWords + 1;
+            }
+
+            return badWords;
+
+        }
+    }
+}
