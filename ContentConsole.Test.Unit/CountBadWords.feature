57e46a7ca0e1239bc236bcb9a6950b94e8b5514c
Gabriel Nepomuceno <gnepomuceno@veinteractive.com>
2015-09-08 02:53:17 +0100

    Creating the first test for count of negative words
    This test actually contains only the happy path of the count of
    negative words

---
+++ ContentConsole.Test.Unit/CountBadWords.feature
@@ -0,0 +1,12 @@
+﻿Feature: CountBadWords
+	In order to filter the messages
+	I need to be able to count the bad words on it
+
+Background: 
+	Given I have a text parser
+
+@success
+Scenario: No bad words 
+	Given I have the input "This is a good input"
+	When I evaluate the message
+	Then the number of bad words should be 0
---
+++ ContentConsole.Test.Unit/CountBadWordsSteps.cs
@@ -0,0 +1,70 @@
+﻿using Shouldly;
+using System;
+using TechTalk.SpecFlow;
+
+namespace ContentConsole.Test.Unit
+{
+    [Binding]
+    public class CountBadWordsSteps
+    {
+        const string INPUT_KEY = "input";
+        const string BAD_WORDS_COUNT_KEY = "badwordscount";
+        private TextParser _textParser
+        {
+            get
+            {
+                return ScenarioContext.Current.Get<TextParser>();
+            }
+            set
+            {
+                ScenarioContext.Current.Set(value);
+            }
+        }
+        private string _input
+        {
+            get
+            {
+                return ScenarioContext.Current.Get<string>(INPUT_KEY);
+            }
+            set
+            {
+                ScenarioContext.Current.Set(value,INPUT_KEY);
+            }
+        }
+        private int _badWordsCount
+        {
+            get
+            {
+                return ScenarioContext.Current.Get<int>(BAD_WORDS_COUNT_KEY);
+            }
+            set
+            {
+                ScenarioContext.Current.Set(value, BAD_WORDS_COUNT_KEY);
+            }
+        }
+
+        [Given(@"I have a text parser")]
+        public void GivenIHaveATextParser()
+        {
+            _textParser = new TextParser();
+        }
+
+        [Given(@"I have the input ""(.*)""")]
+        public void GivenIHaveTheInput(string input)
+        {
+            _input = input;
+        }
+
+        [When(@"I evaluate the message")]
+        public void WhenIEvaluateTheMessage()
+        {
+            _badWordsCount = _textParser.CountBadWords(_input);
+        }
+
+        [Then(@"the number of bad words should be (.*)")]
+        public void ThenTheNumberOfBadWordsShouldBe(int numberBadWords)
+        {
+            _badWordsCount.ShouldBe(numberBadWords);
+        }
+    }
+}
